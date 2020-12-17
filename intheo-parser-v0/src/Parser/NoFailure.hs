6b1f4fb3478e57d0defc17269e8f4132de054a7e
Hexirp <Hexirp@users.noreply.github.com>
2020-12-17 16:28:52 +0900

    Fix: いろいろ直す
    ビルドを通すために

--- intheo-parser-v0/src/Parser/NoFailure.hs
+++ intheo-parser-v0/src/Parser/NoFailure.hs
@@ -19,7 +19,7 @@ module Parser.NoFailure where
       i1 f x = \s c -> x s (\xv xp xs -> c (f xv) xp xs)
 
   instance Monoid p => Applicative (Parser p s m) where
-    pure :: forall p s m. Monoid p => forall a. a -> Parser p s m a
+    pure :: forall p s m a. Monoid p => a -> Parser p s m a
     pure x = i0 x
      where
       i0 :: a -> Parser p s m a
@@ -27,7 +27,7 @@ module Parser.NoFailure where
       i1 :: a -> (forall r. s -> (a -> p -> s -> m r) -> m r)
       i1 x = \s c -> c x mempty s
 
-    (<*>) :: forall p s m. Monoid p => forall a b. Parser p s m (a -> b) -> Parser p s m a -> Parser p s m b
+    (<*>) :: forall p s m a b. Monoid p => Parser p s m (a -> b) -> Parser p s m a -> Parser p s m b
     x0 <*> x1 = i0 x0 x1
      where
       i0 :: Parser p s m (a -> b) -> Parser p s m a -> Parser p s m b
@@ -40,7 +40,7 @@ module Parser.NoFailure where
       i3 x0v x0p x1v x1p x1s c = c (x0v x1v) (x0p <> x1p) x1s
 
   instance Monoid p => Monad (Parser p s m) where
-    (>>=) :: forall p s m. Monoid p => forall a b. Parser p s m a -> (a -> Parser p s m b) -> Parser p s m b
+    (>>=) :: forall p s m a b. Monoid p => Parser p s m a -> (a -> Parser p s m b) -> Parser p s m b
     x0 >>= x1 = i0 x0 x1
      where
       i0 :: Parser p s m a -> (a -> Parser p s m b) -> Parser p s m b
@@ -48,7 +48,7 @@ module Parser.NoFailure where
       i1 :: (forall r. s -> (a -> p -> s -> m r) -> m r) -> (a -> Parser p s m b) -> (forall r. s -> (b -> p -> s -> m r) -> m r)
       i1 x0 x1 = \s c -> x0 s (\x0v x0p x0s -> i2 x0v x0p x0s x1 c)
       i2 :: forall r. a -> p -> s -> (a -> Parser p s m b) -> (b -> p -> s -> m r) -> m r
-      i2 x0v x0p x0s x1 c = i3 x0p (x1 x0v) c
+      i2 x0v x0p x0s x1 c = i3 x0p x0s (x1 x0v) c
       i3 :: forall r. p -> s -> Parser p s m b -> (b -> p -> s -> m r) -> m r
       i3 x0p x0s (Parser x1) c = i4 x0p x0s x1 c
       i4 :: forall r. p -> s -> (forall r. s -> (b -> p -> s -> m r) -> m r) -> (b -> p -> s -> m r) -> m r
